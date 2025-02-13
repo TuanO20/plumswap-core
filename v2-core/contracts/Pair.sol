8ff615644f28874a8af489695ee2926a273dd9bd
TuanO20 <trananhtuancancer@gmail.com>
2025-02-13 23:05:46 +0700

    Update code Pair.sol

--- v2-core/contracts/Pair.sol
+++ v2-core/contracts/Pair.sol
@@ -51,6 +51,16 @@ contract Pair is IUniswapV2Pair, ERC20 {
         token1 = _token1;
     }
 
+    // abi.encodeWithSelector(SELECTOR, to, value) -> To safely call the function    
+    // abi.encode -> Data encoding/packing
+    // abi.encodeWithSelector -> Call the function at the low level    
+    function _safeTransfer(address token, address to, uint value) private {
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
+        // data.length == 0 -> For example: USDT (non-ERC20 token)
+        // abi.decode(data, (bool)) -> Have returned data (ERC20-token)
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "UniswapV2: TRANSFER_FAILED");
+    }
+
     function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
         _reserve0 = reserve0;
         _reserve1 = reserve1;
@@ -60,8 +70,28 @@ contract Pair is IUniswapV2Pair, ERC20 {
     //function _update()
 
 
+    // Calculate the amount of LP tokens to mint more for the feeTo address (Uniswap V2 protocol)
+    function _mintFee(uint112 _reserve0, uint112 _reserve1) private returns (bool feeOn) {
+        address feeTo = IUniswapV2Factory(factory).feeTo();
+        feeOn = feeTo != address(0);
+        uint _kLast = kLast; // gas savings
 
+        if (feeOn) {
+            if (_kLast > 0) {
+                uint rootK = Math.sqrt(uint(_reserve0 * _reserve1));
+                uint rootKLast = Math.sqrt(_kLast);
 
+                uint numerator = (rootK - rootKLast) * totalSupply;
+                uint denominator = 5*rootK + rootLast;
+                uint LPTokenMintedMore = numerator / denominator;
+
+                if (LPTokenMintedMore > 0) 
+                    _mint(feeTo, LPTokenMintedMore);
+            }
+        }
+        else if (_kLast != 0)
+            kLast = 0;
+    }
 
     
     
